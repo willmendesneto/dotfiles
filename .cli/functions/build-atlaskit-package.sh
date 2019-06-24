bd0a3aa85a5c0a5d4c04c44565be0c4e29f092c9
Will Mendes <willmendesneto@gmail.com>
2019-06-24 15:39:19 +1000

    Update build-atlaskit-package.sh

--- .cli/functions/build-atlaskit-package.sh
+++ .cli/functions/build-atlaskit-package.sh
@@ -1,28 +1,24 @@
+#!/bin/bash
 #
 # Run Atlaskit build from a individual package
 #
 # How to use
 #
 # $ cd <your-sunbets-project>
-# $ build-atlaskit-package (-n|--name) (-f|--folder)
+# $ build-atlaskit-package (-f|--folder)
 # 
 # EX:
-# build-atlaskit-package --name=global-navigation --folder=packages/core/global-navigation
+# build-atlaskit-package --folder=packages/core/global-navigation
 #
 function build-atlaskit-package() {
 
   ATLASKIT_PACKAGE_FOLDER=""
-  ATLASKIT_PACKAGE_NAME=""
 
   for i in "$@"
   do
     case $i in
         -f=*|--folder=*)
         ATLASKIT_PACKAGE_FOLDER="${i#*=}"
-        shift # past argument=value
-        ;;
-        -n=*|--name=*)
-        ATLASKIT_PACKAGE_NAME="${i#*=}"
         shift # past argument=value
         ;;
         --default)
@@ -34,25 +30,23 @@ function build-atlaskit-package() {
     esac
   done
 
-  if [ "$ATLASKIT_PACKAGE_FOLDER" == "" ] || [ "$ATLASKIT_PACKAGE_NAME" == "" ]; then
+  if [ "$ATLASKIT_PACKAGE_FOLDER" == "" ]; then
     echo "Please pass a name and a folder for the build command";
-    echo "build-atlaskit-package (-n|--name) (-f|--folder)";
+    echo "build-atlaskit-package (-f|--folder)";
     echo "";
     echo "EX:";
-    echo "build-atlaskit-package --name=global-navigation --folder=packages/core/global-navigation";
+    echo "build-atlaskit-package --folder=packages/core/global-navigation";
     echo "";
   else
   
-    package=@atlaskit/$ATLASKIT_PACKAGE_NAME
-
     echo "Running bundle for ${package} ..."
 
     echo ">>> Removing old bundle ..."
     rm -rf "${ATLASKIT_PACKAGE_FOLDER}/dist/{cjs,esm}"
-    echo ">>> Starting bundle for CJS and ESM format ..."
-    NODE_ENV=production BABEL_ENV=production:cjs bolt workspaces exec --only $package -- babel src -d dist/cjs
+    echo ">>> Starting bundle for CJS format ..."
+    NODE_ENV=production BABEL_ENV=production:esm bolt workspaces exec --parallel --only-fs $ATLASKIT_PACKAGE_FOLDER -- babel src -d dist/esm --root-mode upward
     echo ">>> Starting bundle for ESM format ..."
-    NODE_ENV=production BABEL_ENV=production:esm bolt workspaces exec --only "$package" -- babel src -d dist/esm 
+    NODE_ENV=production BABEL_ENV=production:cjs bolt workspaces exec --parallel --only-fs $ATLASKIT_PACKAGE_FOLDER -- babel src -d dist/cjs --root-mode upward
   fi;
 
   echo "All done!";
