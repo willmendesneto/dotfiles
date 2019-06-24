40664f72ff692daa596c7664f3325973455f9e8d
Will Mendes <willmendesneto@gmail.com>
2019-06-24 15:42:22 +1000

    Create regenerate-npm-token.sh

---
+++ .cli/functions/regenerate-npm-token.sh
@@ -0,0 +1,12 @@
+function regenerate-npm-token() {
+  if [[ -n "$1" ]]; then
+    echo "Let's clean .npm and .npmrc"
+    rm -rf ~/.npm/* ~/.npmrc
+    echo "Now I will fetch your new NPM token, when asked for a password"
+    echo "follow this instructions https://hello.atlassian.net/wiki/spaces/RELENG/pages/137953491/HOWTO+-+Generate+an+Artifactory+API+Key+for+packages.atlassian.com"
+    echo "to get your API key"
+    curl -u $1 https://packages.atlassian.com/api/npm/atlassian-npm/auth/atlassian > ~/.npmrc
+  else
+    echo "proper usage: regenerate-npm-token <your-npm-username>:<your-npm-token>."
+  fi
+}
