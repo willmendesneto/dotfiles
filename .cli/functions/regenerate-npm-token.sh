ea5430a33dd8f6c66c995b2b88a5cdd8f9f2d85d
Will Mendes <willmendesneto@gmail.com>
2020-03-02 14:08:17 +1100

    Update regenerate-npm-token.sh

--- .cli/functions/regenerate-npm-token.sh
+++ .cli/functions/regenerate-npm-token.sh
@@ -6,6 +6,7 @@ function regenerate-npm-token() {
     echo "follow this instructions https://hello.atlassian.net/wiki/spaces/RELENG/pages/137953491/HOWTO+-+Generate+an+Artifactory+API+Key+for+packages.atlassian.com"
     echo "to get your API key"
     curl -u $1 https://packages.atlassian.com/api/npm/atlassian-npm/auth/atlassian > ~/.npmrc
+    echo "https://github.com/willmendesneto/dotfiles/blob/master/.cli/functions/regenerate-npm-token.sh" >> ~/.npmrc
   else
     echo "proper usage: regenerate-npm-token <your-npm-username>:<your-npm-token>."
   fi
