3a7b346f0aa814da380d9ddf1436be7a8e72e598
Will Mendes <willmendesneto@gmail.com>
2020-11-21 23:31:59 +1100

    fix: removing --force option in brew cleanup

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ then
 else
   log "Cleaning up brew…"
   rm -rf ~/Brewfile && cp -rf ~/dotfiles/Brewfile ~/Brewfile
-  brew cleanup --force -s
+  brew cleanup --prune -s
 fi
 
 log "Installing brew dependencies…"
