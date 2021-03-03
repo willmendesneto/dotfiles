6da7b6a8dcef5194e2b2f102deb39f22eb06e19c
Will Mendes <willmendesneto@gmail.com>
2021-03-03 02:20:06 -0300

    Update ga.sh

--- .cli/aliases/ga.sh
+++ .cli/aliases/ga.sh
@@ -1,2 +1,3 @@
 alias gd="git diff --color | diff-so-fancy"
 alias gdca="git diff --cached | diff-so-fancy"
+alias smartlog="git log --graph --pretty=format:'commit: %C(bold red)%h%Creset %C(red)<%H>%Creset %C(bold magenta)%d %Creset%ndate: %C(bold yellow)%cd %Creset%C(yellow)%cr%Creset%nauthor: %C(bold blue)%an%Creset %C(blue)<%ae>%Creset%n%C(cyan)%s%n%Creset'"
